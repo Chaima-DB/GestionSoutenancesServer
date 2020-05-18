9a310bb371cf6a06f64e8b620a6bdd7d990c96f2
Chaima-DB <chimox751@gmail.com>
2020-05-18 00:42:39 +0000

    create back-end of project Gestion des soutenances

---
+++ backEnd/gestionDesSoutenances/src/main/java/com/zsmart/gestionDesSoutenances/bean/StructureDeRecherche.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.zsmart.gestionDesSoutenances.bean;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author HP
+ */
+@Entity
+public class StructureDeRecherche implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String responsable;
+    private String emailRespo;
+    
+    @ManyToOne
+    private Etablissement etablissement;
+
+    public StructureDeRecherche() {
+    }
+             
+
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getResponsable() {
+        return responsable;
+    }
+
+    public void setResponsable(String responsable) {
+        this.responsable = responsable;
+    }
+
+    public String getEmailRespo() {
+        return emailRespo;
+    }
+
+    public void setEmailRespo(String emailRespo) {
+        this.emailRespo = emailRespo;
+    }
+
+    public Etablissement getEtablissement() {
+        return etablissement;
+    }
+
+    public void setEtablissement(Etablissement etablissement) {
+        this.etablissement = etablissement;
+    }
+
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof StructureDeRecherche)) {
+            return false;
+        }
+        StructureDeRecherche other = (StructureDeRecherche) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.zsmart.gestionDesSoutenances.bean.StructureDeRecherche[ id=" + id + " ]";
+    }
+    
+}
